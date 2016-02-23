28e1c914f53d237a50c5805d4d805976085805c9
rvgorlov <rv.gorlov@gmail.com>
2016-02-23 17:10:55 +0600

    add time watchdog

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -7,23 +7,25 @@
 
 using namespace std;
 
-/*class SLAU
+class SLAU
 {
 public:
-	SLAU(arguments);
+	SLAU(unsigned int N) {
+
+	}
 	~SLAU();
 
-};*/
+
+};
 
 class inputUnsInt
 {
 public:
 	inputUnsInt();
 	~inputUnsInt();
-
+	
 	static unsigned int inputUnsIntCheck () {
 	unsigned int size = 0;
-	// Ввод числа с проверкой
 	std::ios::iostate state;
     do {
 		int temp; 
@@ -44,14 +46,25 @@ public:
 		size = temp;
 	} while (state != std::ios::goodbit);
     std::cout << "Вы ввели: " << size << endl;
-
     return size; 
 	};	
 };
+
+
 int main(int argc, char **argv)
 {
-	unsigned int size = inputUnsInt::inputUnsIntCheck(); 
 	
+
+	unsigned int start_time =  clock(); // начальное время
+    
+	/*somecode*/
+
+	unsigned int size = inputUnsInt::inputUnsIntCheck(); 
+
+    unsigned int end_time = clock(); // конечное время
+    unsigned int search_time = end_time - start_time; // искомое время
+	
+    cout << "Время выполнения скрипта: " << search_time << "мс." << endl; 
 
 	return 0;
 }
