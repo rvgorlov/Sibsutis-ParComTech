7c2a8920ee852bf380eea21c1930914e4fe963d1
rvgorlov <rv.gorlov@gmail.com>
2016-04-06 12:24:42 +0600

    final 1 ver

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,34 +1,26 @@
-//clear; g++ -pthread -Wl,--no-as-needed -std=c++11 -Wall -o main main1.cpp
 //clear; valgrind --tool=helgrind --history-level=approx ./main
 //valgrind -v --leak-check=full --show-leak-kinds=all ./main
-
 // g++ -pthread -Wl,--no-as-needed -lboost_system -lboost_thread -std=c++11 -Wall -o main main.cpp; valgrind --tool=helgrind --history-level=approx ./main
-
 // g++ -pthread -Wl,--no-as-needed -lboost_system -lboost_thread -std=c++11 -Wall -o main main.cpp; 
 
 
 #include <iostream>
-#include <pthread.h>
 #include <thread>
-#include <mutex>
 #include <vector>
 #include <fstream>
 #include <iomanip>
-#include <cmath>
-
 #include <boost/thread/thread.hpp>
 #include <boost/thread/barrier.hpp>
+#include <chrono>
 
 using namespace std;
 
-mutex coutMutex;
 double **_arraySlau;
 int _size, MAX_THREAD;
 
 
-double randDouble (double fMin, double fMax) {
-	double f = (double)rand() / RAND_MAX;
- 	return fMin + f * (fMax - fMin);
+double randDouble (double fMin, double fMax) {	
+ 	return fMin + (double)rand() / RAND_MAX * (fMax - fMin);
 }
 
 void printArr (double **arr, int size ) {
@@ -39,7 +31,7 @@ void printArr (double **arr, int size ) {
 		cout.setf(ios::fixed);
 		for (int i = 0; i < size; ++i)
 		{	
-			cout << i << ") "; 
+			cout << setw(2) << left << i << ") "; 
 			for (int j = 0; j < size + 1; ++j)
 			{	
 				if (j == size) cout << "|"; 
@@ -69,8 +61,7 @@ void writeToFile (double **arraySlau, int size) {
 vector<double> getAnswers (double **arraySlau, int size) {
 
 	cout << "Метод Гауса - без thread:" << endl; 
-
-	double start_time =  clock() / (CLOCKS_PER_SEC / 100);
+	auto start_t = std::chrono::high_resolution_clock::now();
 
 	double **arr;
 	arr = new double*[size];  
@@ -93,35 +84,25 @@ vector<double> getAnswers (double **arraySlau, int size) {
  		}
  	}
 
- 	/*double *x = new double[size];
- 	// обратный ход - подставляем и считаем
- 	for (int i = size - 1; i >= 0; i--) {
- 		x[i] = arr[i][size];
- 		for (int j = i + 1; j < size; j++) x[i] -= arr[i][j] * x[j];
- 		x[i] = x[i] / arr[i][i];
- 	}*/
-
  	arr[size - 1][size] /= arr[size - 1][size - 1]; 
  	for (int i = size - 2; i >= 0; i--) {
 		for (int j = i + 1; j < size; j++)
  			arr[i][size] -= arr[i][j] * arr[j][size]; 
  		arr[i][size] /= arr[i][i];
  	}
- 	printArr(arr, size);
+ 	//printArr(arr, size);
  	
 	vector<double> _answers;	
 	for (int i = 0; i < size; ++i) 
 		_answers.push_back(arr[i][size]); 
 	
-	double end_time = clock() / (CLOCKS_PER_SEC / 100);
-    double search_time = end_time - start_time; 
-	
-    cout << "Время = " << search_time << "мс." << endl; 
+    auto end_t = std::chrono::high_resolution_clock::now();
+    auto time = std::chrono::duration<double, std::milli>(end_t-start_t).count();
+   	cout << "Время = " << time << "мс." << endl; 
 
     for (int i = 0; i < size; ++i)
     	delete[] arr[i]; 
     delete[] arr; 
-    //delete[] x;
 	
 	return _answers;  
 }
@@ -149,15 +130,15 @@ void threadCalculateString (double **arr, int size, int start, int end, boost::b
 		}
 		cur_barier.wait();
  	}
-
-	
- 	}
+ }
 
 vector<double> getAnswers (double **arraySlau, int size, int MAX_THREAD) {
 
 	cout << "Метод Гауса - с использованием thread:" << endl; 
-	
-	double start_time =  clock() / (CLOCKS_PER_SEC / 100);
+	auto start_t = std::chrono::high_resolution_clock::now();
+	vector<double> _answers;
+	vector<thread> threads;
+    boost::barrier bar(MAX_THREAD), phase1(MAX_THREAD);
 
 	double **arr = new double*[size];  
 	for (int i = 0; i < size; i++) { 
@@ -166,15 +147,14 @@ vector<double> getAnswers (double **arraySlau, int size, int MAX_THREAD) {
 			arr[i][j] = arraySlau[i][j];
 		}  
 	}
-    vector<thread> threads;
-    boost::barrier bar(MAX_THREAD), phase1(MAX_THREAD);
 
-    int temp = size / (MAX_THREAD - 1);  
+    int temp = size / (MAX_THREAD - 1);  			//для определения интервалов для потоков
 
  	for (int i = 0; i < (MAX_THREAD - 1); ++i) {
     	int start = temp * i, end = temp * (i + 1); 
 		threads.emplace_back (threadCalculateString, ref(arr), size, start, end, boost::ref(bar), boost::ref(phase1)); 	
  	} 
+ 	
  	int start, end;
 
  	if (size % MAX_THREAD - 1 != 0)
@@ -183,7 +163,7 @@ vector<double> getAnswers (double **arraySlau, int size, int MAX_THREAD) {
  		start = temp * (MAX_THREAD - 1); 
  		end = size; 
  		for (int k = 1; k < size; ++k)
-		{		
+		{	
 			for (int j = k; j < end; ++j)
 			{
 				if ((j >= start) && (j < end))
@@ -204,50 +184,36 @@ vector<double> getAnswers (double **arraySlau, int size, int MAX_THREAD) {
  	
  	if (size % MAX_THREAD - 1 != 0)
  	{
- 	start = temp * (MAX_THREAD - 1); /*cout << start << endl; */
- 	end = size; /*cout << end << endl; */
+ 		start = temp * (MAX_THREAD - 1);
+ 		end = size;
 
-	for (int i = size - 2; i >= 0; i--) {
-		if ((i >= start) && (i < end)) {
- 			for (int j = i + 1; j < size; j++)
- 				arr[i][size] -= arr[i][j] * arr[j][size]; 
- 			arr[i][size] /= arr[i][i];
-		}
-		bar.wait();
- 	}
-
+		for (int i = size - 2; i >= 0; i--) {
+			if ((i >= start) && (i < end)) {
+ 				for (int j = i + 1; j < size; j++)
+ 					arr[i][size] -= arr[i][j] * arr[j][size]; 
+ 				arr[i][size] /= arr[i][i];
+			}
+			bar.wait();
+ 		}
  	} 
 
  	for (auto& thread : threads) {
     	if (thread.joinable()){
       		thread.join();
       	}
-    } 
-    	
-	//threads.clear();
- 	printArr(arr, size);
-	
- 	// обратный ход - подставляем и считаем
- 	/*for (int i = size - 1; i >= 0; i--) {
- 		x[i] = arr[i][size];
- 		for (int j = i + 1; j < size; j++) 
- 			x[i] -= arr[i][j] * x[j];
- 		x[i] =x[i] / arr[i][i];
- 	}*/
-	
-    
-	vector<double> _answers;	
+    } 	
 	
 	for (int i = 0; i < size; ++i) 
 		_answers.push_back(arr[i][size]); 
 
-	double end_time = clock() / (CLOCKS_PER_SEC / 100);
-    double search_time = end_time - start_time; 
-    cout << "Время = " << search_time << "мс." << endl; 
+    auto end_t = std::chrono::high_resolution_clock::now();
+    auto time = std::chrono::duration<double, std::milli>(end_t-start_t).count();
+   	cout << "Время = " << time << "мс." << endl; 
 
     for (int i = 0; i < size; ++i)
     	delete[] arr[i]; 
     delete[] arr; 
+	
 	return _answers;  
 }
 
@@ -255,18 +221,12 @@ bool is_equalDouble(double x, double y) {
     return std::fabs(x - y) < std::numeric_limits<double>::epsilon();
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
 	
-bool yesOrNot = false;
-
- //for (int i = 10; i < 1000; i += 100){
-
- 	cout << "====================================================================" << endl; 
-	
-	int _size = 2500;	
-	  
+	bool yesOrNot = false;	
+	int _size = 500;					// Размер матрицы size*size. 
 	_arraySlau = new double*[_size];
+	
 	for (int i = 0; i < _size; i++) {
 		_arraySlau[i] = new double[_size + 1]; 
 		for (int j = 0; j < _size + 1; j++) {
@@ -275,40 +235,39 @@ bool yesOrNot = false;
 	}
 
 	//writeToFile(_arraySlau, _size); 
-
 	printArr(_arraySlau, _size); 
+
 	cout << "Размер матрицы " << _size << " X " << _size + 1 << endl;
 	MAX_THREAD = std::thread::hardware_concurrency(); 
-	cout << "Всего доступно одновременно - " << MAX_THREAD << " потока"<< endl;   
+	cout << "Всего доступно одновременно - " << MAX_THREAD << " потока(ов)"<< endl;   
 
-	vector<double> answer1 = getAnswers(_arraySlau, _size);
-	vector<double> answer2 = getAnswers(_arraySlau, _size, MAX_THREAD);
+	vector<double> answer1 = getAnswers(_arraySlau, _size);					// Последовательный алгоритм
+	vector<double> answer2 = getAnswers(_arraySlau, _size, MAX_THREAD);		// Паралельный алгоритм 
+
 	cout << "Ответы:|без threads | c threads" << "| Всего ответов - " << answer1.size() << endl;
 	std::vector<double>::iterator ii = answer2.begin();
 	int idx = 0; 
 	 
-	for (std::vector<double>::iterator i = answer1.begin(); i != answer1.end(); ++i, ++ii)
-	{	
-		if (idx < 16)
-		{
+	for (std::vector<double>::iterator i = answer1.begin(); i != answer1.end(); ++i, ++ii) {	
+		if (idx < 16) {
 			cout << setw(1) << left << "X" 
 			<< setw(2) << left << idx + 1 << " =  |" 
 			<< setw(12) << left << setprecision(3) << *i << "| "
 			<< setw(8) << left << setprecision(3) << *ii  << " |"<< endl;
 			idx++;
-		 }
-		if (is_equalDouble(round(*i * 100) /100, round(*ii * 100) / 100) != true){
+		}
+		if (is_equalDouble(round(*i * 100) /100, round(*ii * 100) / 100) != true) {
 			yesOrNot = true; 
 		}
 	}
 
-	for (int i = 0; i < _size; ++i)
-    	delete[] _arraySlau[i]; 
-    delete[] _arraySlau; 
-
- //}	
-if (yesOrNot)
+	if (yesOrNot)
 		cout << "Проверка не пройдена" << endl;	
 	else cout << "\n   === Проверка пройдена \n"<< endl;
+
+	for (int i = 0; i < _size; ++i)
+    	delete[] _arraySlau[i]; 
+    delete[] _arraySlau;
+	
 	return 0;
 }
