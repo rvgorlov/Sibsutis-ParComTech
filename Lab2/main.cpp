00b163fffbb9c8b55a6093eb276ddfca47207ab9
rvgorlov <rv.gorlov@gmail.com>
2016-04-14 14:09:13 +0600

    add first commit for Lab3

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -8,14 +8,17 @@
 /*
 Для отладки: 
 clear; g++ -pthread -Wl,--no-as-needed -lboost_system -lboost_thread -std=c++11 -Wall -o main main.cpp; ./main
-Для отоладки в потоках: 
+Для отладки в потоках: 
 clear; g++ -pthread -Wl,--no-as-needed -lboost_system -lboost_thread -std=c++11 -Wall -o main main.cpp; valgrind --tool=helgrind --history-level=approx ./main
 */
 
-
+#include <iomanip>
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <vector>
+
+#include <boost/thread/barrier.hpp>
 
 using namespace std;
 
@@ -30,23 +33,22 @@ private:
 
    int _structSize; 
    Node *Head, *Tail;
+   mutex changeMutex, coutMutex; 
 
 public:
    threadList():_structSize(0),Head(NULL),Tail(NULL){}; 
-   
    ~threadList() {
-   
+      unique_lock<mutex> lock (changeMutex);
       while (Head) { 
          Tail = Head->Next;
          delete Head;
          Head = Tail;
       }
-
 }
 
 bool seachNode(T seachX) {
    Node *temp = Head; 
-   
+   unique_lock<mutex> lock (changeMutex);
    if (Tail->x == seachX)
       return true;
 
@@ -59,9 +61,8 @@ bool seachNode(T seachX) {
 }
 
 void deleteX(T deleteX) {
-
    Node *temp = Head; 
-   
+   unique_lock<mutex> lock (changeMutex);
    if (Head != NULL)
    {
       while (temp != Tail) {      
@@ -88,12 +89,12 @@ void deleteX(T deleteX) {
 }
 
 void Add(T x) {
-
+   unique_lock<mutex> lock (changeMutex);
    ++_structSize; 
    Node *temp = new Node;
    temp->Next = NULL;
    temp->x = x;
- 
+   
    if (Head!=NULL)
    {
        temp->Prev = Tail;
@@ -109,6 +110,7 @@ void Add(T x) {
 void Show() {
    Node *temp = Head;
    int idx = 0;  
+   unique_lock<mutex> lock (changeMutex);
    while (temp != NULL) {
       ++idx;
       if (idx == _structSize)
@@ -133,24 +135,77 @@ double randDouble (double fMin, double fMax) {
    return fMin + (double)rand() / RAND_MAX * (fMax - fMin);
 }
 
+void threadWork (threadList<double> &lst1, boost::barrier& startBarrier, mutex &coutMutex, int variable) {
+   startBarrier.wait();
+   auto start_t = std::chrono::high_resolution_clock::now();
+
+
+/*switch (variable) {
+      case 0:
+         lst1.deleteX(20);  
+         break;
+      case 2:
+         lst1.Add(999);
+         break;
+      case 3:
+         lst1.seachNode(999);
+         break;
+      default:
+      break;
+}*/
+   //cout << variable << endl; 
+
+   for (int i = 0; i < 1000000; ++i)
+   {
+      lst1.Add(variable);
+      lst1.seachNode(variable);
+      lst1.deleteX(variable);
+   }
+   
+   auto end_t = std::chrono::high_resolution_clock::now();
+   auto time = std::chrono::duration<double, std::milli>(end_t-start_t).count();
+   unique_lock<mutex> lck(coutMutex); cout << "Время = " << time << "мс." << endl; 
+}
+
 int main(int argc, char const *argv[]){
 
-threadList<double> lst;
+   threadList<double> lst;
+   vector<thread> threads;
 
-for (int i = 0; i < 33; ++i) 
-   lst.Add(randDouble(0.0, 1000.0));   
-lst.Add(20.00);
+   int MAX_THREAD = std::thread::hardware_concurrency();
+   boost::barrier startBarrier(MAX_THREAD);
+   mutex coutMutex; 
 
-cout << lst.getSize() << endl; 
-cout << lst.getHead() << endl;
-cout << lst.getLast() << endl;
+   for (int i = 0; i < 33; ++i) 
+      lst.Add(randDouble(0.0, 1000.0));   
+   //lst.Add(20.00);
+   //lst.Add(999);
 
-lst.deleteX(20);
+   int variable = rand()%3; 
+   for (int i = 0; i < MAX_THREAD; ++i) { 
+      threads.emplace_back (threadWork, ref(lst), ref(startBarrier), ref(coutMutex), variable);    
+   } 
 
-if (lst.seachNode(20)) cout << "Искомый элемент найден" << endl;
-else cout << "Искомый элемент не найден" << endl;
+   for (auto& thread : threads) {
+      if (thread.joinable()){
+            thread.join();
+         }
+   }
 
-lst.Show(); //Отображаем список на экране
+   /*for (int i = 0; i < 33; ++i) 
+      lst.Add(randDouble(0.0, 1000.0));   
+   lst.Add(20.00);*/
+
+   /*cout << lst.getSize() << endl; 
+   cout << lst.getHead() << endl;
+   cout << lst.getLast() << endl;*/
+
+   /*lst.deleteX(20);*/
+
+   if (lst.seachNode(20)) cout << "Искомый элемент найден" << endl;
+   else cout << "Искомый элемент не найден" << endl;
+
+   lst.Show();
    
 	return 0;
 }
---
+++ Lab3/main.cpp
@@ -0,0 +1,34 @@
+/*
+==============================
+Реализация редукции для 
+произволных операций над
+массивом
+==============================
+*/
+
+/*
+Для отладки: 
+clear; g++ -pthread -Wl,--no-as-needed -lboost_system -lboost_thread -std=c++11 -Wall -o main main.cpp; ./main
+Для отладки в потоках: 
+clear; g++ -pthread -Wl,--no-as-needed -lboost_system -lboost_thread -std=c++11 -Wall -o main main.cpp; valgrind --tool=helgrind --history-level=approx ./main
+*/
+
+#include <iomanip>
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+
+#include <boost/thread/barrier.hpp>
+
+using namespace std;
+
+double randDouble (double fMin, double fMax) {  
+   return fMin + (double)rand() / RAND_MAX * (fMax - fMin);
+}
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
